5990990ae3ede0608a6d81c4947d53a41ec96cdc
Justin Horton <justin.t.horton@aexp.com>
2021-01-31 22:37:43 -0700

    feat(games_api): setup initial gamesapi json

--- src/Components/Games/DnD/DnD.js
+++ src/Components/Games/DnD/DnD.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import gameApi from '../../../gamesApi/gamesApi.json';
+import '../games.css';
 
 function DnD() {
+  const gameInfo = gameApi.game.DnD.campaigns;
+  console.log('gameInfo', gameInfo);
+
+  const games = gameInfo.map(e => {
+    const { image, title, description } = e.campaign;
+    return (
+      <div className="card">
+        <img src={image} />
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <button>Play "{title}"</button>
+      </div>
+    );
+  });
+
   return (
     <div className="DnD-component">
-      <div className="DnD">DnD</div>
+      <div className="DnD">{games}</div>
     </div>
   );
 }
